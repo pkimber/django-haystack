4f955d74995027bb709933f58940a8f1869602cd
Patrick Kimber <code@pkimber.net>
2014-02-25 22:26:16 +0000

    More porting to Py3.

--- haystack/utils/__init__.py
+++ haystack/utils/__init__.py
@@ -24,11 +24,11 @@ def default_get_identifier(obj_or_string):
     """
     if isinstance(obj_or_string, six.string_types):
         if not IDENTIFIER_REGEX.match(obj_or_string):
-            raise AttributeError(u"Provided string '%s' is not a valid identifier." % obj_or_string)
+            raise AttributeError("Provided string '%s' is not a valid identifier." % obj_or_string)
 
         return obj_or_string
 
-    return u"%s.%s.%s" % (
+    return "%s.%s.%s" % (
         obj_or_string._meta.app_label,
         obj_or_string._meta.module_name,
         obj_or_string._get_pk_val()
@@ -52,13 +52,13 @@ def _lookup_identifier_method():
     try:
         module = importlib.import_module(module_path)
     except ImportError:
-        raise ImportError(u"Unable to import module '%s' provided for HAYSTACK_IDENTIFIER_METHOD." % module_path)
+        raise ImportError("Unable to import module '%s' provided for HAYSTACK_IDENTIFIER_METHOD." % module_path)
 
     identifier_method = getattr(module, method_name, None)
 
     if not identifier_method:
         raise AttributeError(
-            u"Provided method '%s' for HAYSTACK_IDENTIFIER_METHOD does not exist in '%s'." % (method_name, module_path)
+            "Provided method '%s' for HAYSTACK_IDENTIFIER_METHOD does not exist in '%s'." % (method_name, module_path)
         )
 
     return identifier_method
